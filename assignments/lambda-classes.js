664eaf3db29fc4d526e14aba74d0a3ffddc9fd16
George Kando <gkando@gmail.com>
2019-05-09 13:32:52 -0500

    second commit - 98% complete - will attempt stretch

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,53 +1,55 @@
 // CODE here for your Lambda Classes
 
-/* 
-* Lambda personnel can be broken down into three different types of `people`.
-  * **Instructors** - extensions of Person
-  * **Students** - extensions of Person
-  * **Project Managers** - extensions of Instructors
-* **IMPORTANT** - You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes. For example:
+ class Person {
+     constructor(props) {
+         this.name = props.name
+         this.age = props.age
+         this.location = props.location
+         this.gender = props.gender
+     }
+     speak() {
+         return `Hello my name is ${this.name}, I am from ${this.location}.`;
+     }
+ }
 
-const fred = new Instructor({
-  name: 'Fred',
-  location: 'Bedrock',
-  age: 37,
-  gender: 'male',
-  favLanguage: 'JavaScript',
-  specialty: 'Front-end',
-  catchPhrase: `Don't forget the homies`
-});
+class Instructor extends Person {
+    constructor(props) {
+        super(props)
+        this.specialty = props.specialty
+        this.favLanguage = props.favLanguage
+        this.catchPhrase = props.catchPhrase
+    }
 
+    demo(subject) {
+        return `Hi! I am your instructor ${this.name}. Today we are learning about ${subject}.`;
+    }
 
-#### Person
-* First we need a Person class. This will be our `base-class`
-* Person receives `name` `age` `location` `gender` all as props
-* Person receives `speak` as a method.
-* This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`
+    }
+}
 
-#### Instructor
-* Now that we have a Person as our base class, we'll build our Instructor class.
-* Instructor uses the same attributes that have been set up by Person
-* Instructor has the following unique props:
-  * `specialty` what the Instructor is good at i.e. 'redux'
-  * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-  * `catchPhrase` i.e. `Don't forget the homies`
-* Instructor has the following methods:
-  * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-  * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
+    class Student extends Person {
+        constructor(props) {
+            super(props)
+            this.previousBackground = props.previousBackground 
+            this.className = props.className
+            this.favSubjects = props.favSubjects
+        }
 
-#### Student
-* Now we need some students!
-* Student uses the same attributes that have been set up by Person
-* Student has the following unique props:
-  * `previousBackground` i.e. what the Student used to do before Lambda School
-  * `className` i.e. CS132
-  * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-* Student has the following methods:
-  * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-  * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-  * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+    listsSubjects() {
+        return this.favSubjects
+    }
+    // a method that logs out all of the student's favoriteSubjects one by one.
+        PRAssignment(subject) {
+            return `${this.name} has submitted a PR for ${subject}`
+        }
+        sprintChallenge(subject) {
+            return `${this.name} has begun sprint challenge on ${subject}`
+        }
+    }
 
-#### Project Manager
+/* #### Project Manager
 * Now that we have instructors and students, we'd be nowhere without our PM's
 * ProjectManagers are extensions of Instructors
 * ProjectManagers have the following unique props:
@@ -57,3 +59,121 @@ const fred = new Instructor({
   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
  */
+
+ class ProjectManager extends Instructor{
+     constructor(props) {
+        super(props)
+        this.gradClassName = props.gradClassName
+        this.favInstructor = props.favInstructor
+     }
+     standup(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+     }
+
+     debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+     }
+ }
+
+ const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+const alex = new Instructor({
+    name: 'Alex',
+    location: 'Montana',
+    age: 33,
+    gender: 'male',
+    favLanguage: 'Scala',
+    specialty: 'Back-end',
+    catchPhrase: `Eat Sleep Code Repeat`
+});
+
+const napoleon = new Instructor({
+    name: 'Napoleon',
+    location: 'Idaho',
+    age: 34,
+    gender: 'male',
+    favLanguage: 'C++',
+    specialty: 'Dev Ops',
+    catchPhrase: `Just dance`
+  });
+
+  const bob = new Student({
+    name: 'Bob',
+    location: 'Bobsville',
+    age: 34,
+    gender: 'male',
+    previousBackground: 'General Contractor',
+    className: 'Web20',
+    favSubjects: 'Constructors'
+  });
+
+  const abe = new Student({
+    name: 'Abe',
+    location: 'Illinois',
+    age: 55,
+    gender: 'male',
+    previousBackground: 'President',
+    className: 'DS8',
+    favSubjects: 'Regression'
+  });
+
+  const buzz = new Student({
+    name: 'Buzz',
+    location: 'Moon',
+    age: 75,
+    gender: 'male',
+    previousBackground: 'Astronaut',
+    className: 'UI/UX',
+    favSubjects: 'Material Design'
+  });
+
+  const kelly = new ProjectManager({
+    name: 'Kelly',
+    location: 'Bayside',
+    age: 28,
+    gender: 'female',
+    gradClassName: 'Web17',
+    favInstructor: 'Dan',
+  });
+
+  const josh = new ProjectManager({
+    name: 'Josh',
+    location: 'California',
+    age: 37,
+    gender: 'male',
+    gradClassName: 'Handsome Guys 101',
+    favInstructor: 'Ashton',
+  });
+
+  const sabrina = new ProjectManager({
+    name: 'Sabrina',
+    location: 'Witchville',
+    age: 25,
+    gender: 'female',
+    gradClassName: 'DS2',
+    favInstructor: 'Craig',
+  });
+
+  console.log(fred.speak());
+  console.log(fred.demo("JS IV"))
+  console.log(fred.grade(buzz, "CSS"))
+
+  console.log(alex.speak());
+  console.log(alex.demo("Multi-threading"))
+  console.log(alex.grade(bob, "Python"))
+
+  console.log(napoleon.speak());
+  console.log(napoleon.demo("Dev Ops"))
+  console.log(napoleon.grade(abe, "Algorithims"))
+
+  console.log(bob.speak());
+  console.log(bob.PRAssignment("Python"))
+  console.log(bob.sprintChallenge("jQuery"))
